b4a4da3d120a553e374fe22375662fb66e409621
Unknown <ad.betin@uniandes.edu.co>
2018-01-19 06:50:53 -0500

    Creación componentes base angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HotelDetailsComponent } from './hotels/hotel-details/hotel-details.component';
+import { HotelListComponent } from './hotels/hotel-list/hotel-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelDetailsComponent,
+    HotelListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hotels/hotel.ts
@@ -0,0 +1,8 @@
+export class Hotel {
+    _id?: string;
+    name: string;
+    stars: number;
+    price: number;
+    image: string;
+    amenities: Array<string>;
+}
