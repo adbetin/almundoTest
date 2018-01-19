4302dfe48a32b27dae599e36fe9d0d7447dcf0ba
Unknown <ad.betin@uniandes.edu.co>
2018-01-19 06:35:25 -0500

    Corrección url mongodb

--- server.js
+++ server.js
@@ -11,8 +11,14 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
+var mongoURI = process.env.MONGODB_URI;
+
+if(!mongoURI){
+    mongoURI = 'mongodb://heroku_x4tjb2tf:f0hcnsenq2q9ov1ikta8kfp3nk@ds139979.mlab.com:39979/heroku_x4tjb2tf';
+}
+
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(mongoURI, function (err, database) {
     if (err) {
         console.log(err);
         process.exit(1);
@@ -75,10 +81,35 @@ app.post("/api/hotels", function (req, res) {
  */
 
 app.get("/api/hotels/:id", function (req, res) {
+    db.collection(HOTELS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get hotel");
+        } else {
+            res.status(200).json(doc);
+        }
+    });
 });
 
 app.put("/api/hotels/:id", function (req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(HOTELS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to update hotel");
+        } else {
+            updateDoc._id = req.params.id;
+            res.status(200).json(updateDoc);
+        }
+    });
 });
 
 app.delete("/api/hotels/:id", function (req, res) {
+    db.collection(HOTELS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function (err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete hotel");
+        } else {
+            res.status(200).json(req.params.id);
+        }
+    });
 });
